4dec6ce7d22efe4c92b20ad95701a265fb8c098a
Denis2389 <bereznyakdenis2003@gmail.com>
2024-08-08 15:18:22 +0300

    part

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log(1295-146);
+// let a = 5;
+// let b = 10;
+
+// [a,b] = [b,a]
+
+// console.log(a,b);
+
+// function isEven(num) {
+//     if (num % 2 === 0) {
+//         console.log('true');
+//     } else {
+//         console.log('false')
+//     }
+// }
+
+// isEven(5)
+
+// function isEven(num) {
+//     return num % 2 === 0;
+// }
+
+// console.log(isEven(3))
+
+
+function findMax(arr) {
+    maxArr = [0]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > maxArr) {
+            maxArr = arr[i]
+        }
+    }
+return maxArr
+}
+
+console.log(findMax([1,3,7,2,5]));
